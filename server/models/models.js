f5b2fb7fdf5220abb7d8e9b0a7e08ac4534cc2bf
ivan sysoev <ikotamsl@gmail.com>
2024-01-30 22:34:22 +0500

    model relations

--- server/models/models.js
+++ server/models/models.js
@@ -4,7 +4,7 @@ const sequelize = require('../db');
 const {DataTypes} = require('sequelize');
 
 const customer = sequelize.define('customer', {
-    customer_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
     first_name: {type: DataTypes.STRING, primaryKey: false},
     second_name: {type: DataTypes.STRING, primaryKey: false},
     patronymic_name: {type: DataTypes.STRING, primaryKey: false},
@@ -14,7 +14,7 @@ const customer = sequelize.define('customer', {
 });
 
 const employee = sequelize.define('employee', {
-    emp_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
     first_name: {type: DataTypes.STRING, primaryKey: false},
     second_name: {type: DataTypes.STRING, primaryKey: false},
     patronymic_name: {type: DataTypes.STRING, primaryKey: false},
@@ -24,20 +24,20 @@ const employee = sequelize.define('employee', {
 });
 
 const admin = sequelize.define('admin', {
-    admin_id : {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
+    id : {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
     login: {type: DataTypes.STRING, primaryKey: false},
     password: {type: DataTypes.STRING, primaryKey: false}
 });
 
 const order = sequelize.define('order', {
-    order_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
     quantity: {type: DataTypes.INTEGER, min: 1, isNull: false},
     comment: {type: DataTypes.STRING, isNull: true},
     is_special: {type: DataTypes.BOOLEAN, isNull: false}
 });
 
 const position = sequelize.define('position', {
-    position_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
     name: {type: DataTypes.STRING, isNull: false},
     type: {type: DataTypes.STRING, isNull: false},
     year: {type: DataTypes.INTEGER, isNull: false},
@@ -46,7 +46,7 @@ const position = sequelize.define('position', {
 });
 
 const address = sequelize.define('address', {
-    address_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
     country: {type: DataTypes.STRING, isNull: false},
     city: {type: DataTypes.STRING, isNull: false},
     house: {type: DataTypes.STRING, isNull: false},
@@ -55,13 +55,39 @@ const address = sequelize.define('address', {
 });
 
 const contact = sequelize.define('contact', {
-    contact_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
     type: {type: DataTypes.STRING, isNull: false},
     string: {type: DataTypes.STRING, isNull: false}
 });
 
 const attribute = sequelize.define('attribute', {
-    attribute_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
     attr_name: {type: DataTypes.STRING, isNull: false},
     attr_value: {type: DataTypes.STRING, isNull: false}
 });
+
+customer.hasMany(contact);
+
+customer.hasMany(address);
+address.belongsTo(customer)
+
+customer.hasMany(order);
+order.belongsTo(customer)
+
+order.hasOne(address);
+order.hasMany(position);
+
+employee.hasMany(contact);
+
+position.hasMany(attribute);
+
+module.exports = {
+    customer,
+    employee,
+    admin,
+    order,
+    position,
+    address,
+    contact,
+    attribute
+}
