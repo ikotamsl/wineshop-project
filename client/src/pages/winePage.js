0c587541516134985b7b4810baa81ac03b59235b
Ivan Sysoev <ivansysoev@webster.edu>
2024-03-18 17:06:56 +0500

    wine page

--- client/src/pages/winePage.js
+++ client/src/pages/winePage.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 
-const WinePage = (position) => {
+const WinePage = () => {
+    const position = {
+        id: 1,
+        name: 'Chevalier Noir',
+        price: 250
+    }
     const attributes = [
         {
             attr_name: "Shore",
@@ -10,35 +15,15 @@ const WinePage = (position) => {
     ]
     return (
         <Container mt={4}>
-            <Row>
-                <Col md={4}>
-                    <Image />
-                </Col>
-
-                <Col md={4}>
-                    <Row>
-                        <h2>{position.name}</h2>
-                    </Row>
-                </Col>
-
-                <Col md={4}>
-                    <Card
-                        className={"d-flex flex-column align-items-center justify-content-around"}
-                        style={{width: 500, height: 500, fontSize: 32, border: '3px solid darkred'}}
-                    >
-                        <h3>{position.price}</h3>
-                        <Button variant={'outline-dark'}>Add to cart</Button>
-                    </Card>
-                </Col>
-            </Row>
-            <Row className={"d-flex flex-column m-5 align-items-end"}>
-                {attributes.map(e =>
-                    <Row>
-                        <h3>{e.attr_name}</h3>
-                        <p>{e.attr_value}</p>
-                    </Row>
-                )}
-            </Row>
+            <h2>{position.name}</h2>
+            {attributes.map(e =>
+                <Row>
+                    <h4>{e.attr_name}</h4>
+                    <p>{e.attr_value}</p>
+                </Row>
+            )}
+            <h3>{position.price}</h3>
+            <Button variant={'outline-dark'}>Add to cart</Button>
         </Container>
     );
 };
